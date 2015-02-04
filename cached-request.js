daa3adf97cb5811724ecb35b49fd7dc2e5f045e5
Atul Varma <varmaa@gmail.com>
2015-02-04 16:09:17 -0500

    cached-request.js now caches first, then sends cached response

--- cached-request.js
+++ cached-request.js
@@ -1,47 +1,89 @@
 var fs = require('fs');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var crypto = require('crypto');
 var mime = require('mime');
 var request = require('request');
+var async = require('async');
 
 var CACHE_DIR = __dirname + '/cache';
+var MAX_KEY_LEN = 80;
 
 if (!fs.existsSync(CACHE_DIR))
   fs.mkdirSync(CACHE_DIR);
 
-function getCachedEntry(keyPath) {
+var urlQueues = {};
 
+function CachedRequest(url) {
+  this.url = url;
+  this.key = encodeURIComponent(url);
+  if (this.key.length > MAX_KEY_LEN) {
+    var hash = crypto.createHash('md5');
+    hash.update(url);
+    this.key = hash.digest('hex');
+  }
+  this.keyPath = CACHE_DIR + '/' + this.key + '.json';
 }
 
-function cacheEntry(url, key, keyPath) {
+util.inherits(CachedRequest, EventEmitter);
+
+CachedRequest.prototype.sendResponse = function() {
+  var metadata = JSON.parse(fs.readFileSync(this.keyPath));
+  var contentPath = CACHE_DIR + '/' + metadata.filename;
+  var response = fs.createReadStream(contentPath);
+  response.statusCode = metadata.statusCode;
+  response.headers = metadata.headers;
+  this.emit('response', response);
+};
+
+function cacheEntry(url, key, keyPath, cb) {
   var proxyReq = request.get(url);
 
   proxyReq.on('response', function(proxyRes) {
-    fs.writeFileSync(keyPath, JSON.stringify({
-      statusCode: proxyRes.statusCode,
-      headers: proxyRes.headers
-    }, null, 2));
-
     var type = proxyRes.headers['content-type'] ||
                'application/octet-stream';
     var ext = mime.extension(type);
+    var filename = key + '.content.' + ext;
+    var contentPath = CACHE_DIR + '/' + filename;
 
-    var contentPath = CACHE_DIR + '/' + key + '.content.' + ext;
+    fs.writeFileSync(keyPath, JSON.stringify({
+      statusCode: proxyRes.statusCode,
+      headers: proxyRes.headers,
+      filename: filename
+    }, null, 2));
 
     proxyRes.pipe(fs.createWriteStream(contentPath))
       .on('close', function() {
-        console.log("DONE WRITING", url);
-      });
-  });
+        cb(null);
+      }).on('error', cb);
+  }).on('error', cb);
 
   return proxyReq;
 }
 
-function get(url) {
-  var key = encodeURIComponent(url);
-  var keyPath = CACHE_DIR + '/' + key + '.json';
+function respond(req, cb) {
+  if (fs.existsSync(req.keyPath)) {
+    req.sendResponse();
+    cb(null);
+  } else {
+    cacheEntry(req.url, req.key, req.keyPath, function(err) {
+      cb(null);
+      if (err)
+        return req.emit('error', err);
+      req.sendResponse();
+    });
+  }
+}
 
-//  if (fs.existsSync(keyPath))
-//    return getCachedEntry(keyPath);
-  return cacheEntry(url, key, keyPath);
+function get(url) {
+  var req = new CachedRequest(url);
+
+  if (!(url in urlQueues))
+    urlQueues[url] = async.queue(respond, 1);
+
+  urlQueues[url].push(req);
+
+  return req;
 }
 
 exports.get = get;
