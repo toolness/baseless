cc41e81ad9dc9220fbda9e3c2c8634d2798d19c9
Atul Varma <varmaa@gmail.com>
2015-02-05 09:40:16 -0500

    root index page now looks like a browser.

--- urls.js
+++ urls.js
@@ -1,3 +1,10 @@
+var urlModule = require('url');
+
 exports.rewriteURL = function(url) {
   return '/proxy?url=' + encodeURIComponent(url);
+};
+
+exports.extractURL = function(url) {
+  var info = urlModule.parse(url, true);
+  return info.query.url;
 };
