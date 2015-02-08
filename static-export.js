a72f7a67c57c1caefddf39a6ceee4609a091c3c1
Atul Varma <varmaa@gmail.com>
2015-02-08 16:33:49 -0500

    static-export.js now has a proper API.

--- static-export.js
+++ static-export.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
 var urlModule = require('url');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 var path = require('path');
+var _ = require('underscore');
 var mkdirp = require('mkdirp');
 var archiver = require('archiver');
+
 var spider = require('./spider');
 var cachedRequest = require('./cached-request');
 var Proxifier = require('./proxifier');
@@ -52,39 +56,27 @@ StaticAssetMap.prototype.filenameFor = function(url) {
 };
 
 function FilesystemArchiver(rootDir) {
+  EventEmitter.apply(this);
   this.rootDir = rootDir;
 }
 
-FilesystemArchiver.prototype.archive = function(filename, stream) {
+util.inherits(FilesystemArchiver, EventEmitter);
+
+FilesystemArchiver.prototype.append = function(stream, options) {
+  var filename = options.name;
   var fullPath = path.normalize(path.join(this.rootDir, filename));
   var dirname = path.dirname(fullPath);
 
   mkdirp.sync(dirname);
-  return stream.pipe(fs.createWriteStream(fullPath));
+  stream.pipe(fs.createWriteStream(fullPath));
 };
 
 FilesystemArchiver.prototype.finalize = function() {
-
+  process.nextTick(this.emit.bind(this, 'finalize'));
 };
 
-function ArchiveFileArchiver(options) {
-  this.options = options;
-  this.archiver = archiver.create(options.format);
-}
-
-ArchiveFileArchiver.prototype.archive = function(filename, stream) {
-  this.archiver.append(stream, {name: filename});
-};
-
-ArchiveFileArchiver.prototype.finalize = function(filename, stream) {
-  this.archiver.finalize();
-};
-
-function main() {
-  var indexURL = 'https://docs.djangoproject.com/en/1.7/';
-  var arch = new ArchiveFileArchiver({
-    format: 'zip'
-  });
+function exportStaticFiles(options) {
+  var self = new EventEmitter();
   var assetMap = new StaticAssetMap();
   var proxifier = new Proxifier({
     rewriteURL: function(url, fromURL, type) {
@@ -102,54 +94,87 @@ function main() {
       if (relPath) relPath += '/';
       relPath += posixPath.basename(toFilename);
 
-//      console.log(relPath);
-
       return relPath;
     },
     formSubmitURL: '',
     request: cachedRequest
   });
-  spider({
-    proxifier: proxifier,
-    url: indexURL,
-    linkPrefix: indexURL,
-    ttl: 0
-  }).on('response', function(res) {
+  var spiderOptions = _.extend({
+    proxifier: proxifier
+  }, _.pick(options, 'url', 'linkPrefix', 'ttl'));
+
+  spider(spiderOptions).on('response', function(res) {
     var req = new cachedRequest.CachedRequest(res.url);
     var drainResponse = function() {
       res.on('data', function() { /* Just drain the stream. */ });
     };
 
     if (!req.isResponseCached()) {
-      console.log("WARNING NO CACHED RESPONSE FOR", res.url);
+      self.emit('warning', "no cached response for " + res.url);
       return drainResponse();
     }
 
     var filename = assetMap.filenameFor(res.url);
 
     if (!filename) {
-      console.log("WARNING NO FILENAME FOR", res.url);
+      self.emit('warning', "no filename for " + res.url);
       return drainResponse();
     }
 
     if (assetMap.isRedirect(res.url))
       return drainResponse();
 
-    arch.archive(filename, res);
-
-//    console.log(res.url, "->", filename);
+    self.emit('append', {
+      url: res.url,
+      stream: res,
+      filename: filename
+    });
+    options.archiver.append(res, {name: filename});
+  }).on('error', function(err) {
+    self.emit('error', err);
   }).on('end', function() {
-    arch.finalize();
-    console.log("done spidering");
+    options.archiver.finalize();
+    self.emit('end');
   });
 
-  assetMap.filenames[indexURL] = 'index.html';
+  assetMap.filenames[options.url] = 'index.html';
 
-  arch.archiver.pipe(fs.createWriteStream('archive.zip'))
-    .on('close', function() {
-      console.log("done writing archive.zip");
-    });
+  return self;
 }
 
+function main(archiveType) {
+  var indexURL = 'https://docs.djangoproject.com/en/1.7/';
+  var arch;
+
+  if (archiveType == 'zip') {
+    arch = archiver.create('zip');
+    arch.pipe(fs.createWriteStream('archive.zip'))
+      .on('close', function() {
+        console.log("done writing archive.zip");
+      });
+  } else if (archiveType == 'fs') {
+    arch = new FilesystemArchiver(__dirname + '/build');
+    arch.on('finalize', function() {
+      console.log("done writing to build/");
+    });
+  } else {
+    throw new Error('unknown archive type: ' + archiveType);
+  }
+
+  exportStaticFiles({
+    archiver: arch,
+    url: indexURL,
+    linkPrefix: indexURL,
+    ttl: 0
+  }).on('warning', function(message) {
+    console.log("WARNING: " + message);
+  }).on('end', function() {
+    console.log("done spidering");
+  });
+}
+
+module.exports = exportStaticFiles;
+module.exports.FilesystemArchiver = FilesystemArchiver;
+
 if (!module.parent)
-  main();
+  main(process.argv[2] || 'zip');
