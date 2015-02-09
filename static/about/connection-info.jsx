41c6af7f3a976b7420cc07a7499bb5fe983b3a7c
Atul Varma <varmaa@gmail.com>
2015-02-09 16:21:34 -0500

    better filtering for loopback hostnames

--- static/about/connection-info.jsx
+++ static/about/connection-info.jsx
@@ -1,22 +1,25 @@
 var _ = require('underscore');
 
 var ConnectionInfo = React.createClass({
+  LOOPBACK_HOSTNAMES: ['localhost', '127.0.0.1'],
+  isHostnameLoopback: function(hostname) {
+    return this.LOOPBACK_HOSTNAMES.indexOf(hostname.toLowerCase()) != -1;
+  },
   render: function() {
     var location = this.props.location;
     var ipconfig = this.props.ipconfig;
 
     var origins = [];
 
-    var LOOPBACK_HOSTNAMES = ['localhost', '127.0.0.1'];
-
-    if (LOOPBACK_HOSTNAMES.indexOf(location.hostname.toLowerCase()) == -1) {
+    if (!this.isHostnameLoopback(location.hostname)) {
       origins.push(location.protocol + '//' + location.host);
     }
 
     ipconfig.hostnames.forEach(function(hostname) {
+      if (this.isHostnameLoopback(hostname)) return;
       origins.push(location.protocol + '//' + hostname + ':' +
                    ipconfig.port);
-    });
+    }, this);
 
     origins = _.uniq(origins);
 
